45eed2b7addaba1e05ceb23317ca87d2200490ca
Adam Lebioda <adam.m.lebioda@gmail.com>
2026-07-13 16:05:03 +0200

    feat(health): add health endpoint to the order service

---
+++ apps/order-service/src/main.ts
@@ -0,0 +1,15 @@
+import 'reflect-metadata';
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module.js';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const port = 3001;
+  await app.listen(port);
+
+  console.log(`order-service listening on port ${port}`);
+}
+
+void bootstrap();
